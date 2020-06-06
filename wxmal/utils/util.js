bc0e3411ab247f56382d13e0bcebfa3c294ad6aa
lovelyz <15969620150a@gmail.com>
2020-06-06 17:08:52 +0800

    小程序 前端页面

---
+++ wxmal/utils/util.js
@@ -0,0 +1,25 @@
+const basePath = 'http://127.0.0.1:8080/';
+const imgViewPath = 'http://127.0.0.1:8080/profile/upload/';
+
+
+const formatTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+module.exports = {
+  formatTime: formatTime,
+  basePath: basePath,
+  imgViewPath: imgViewPath
+}
